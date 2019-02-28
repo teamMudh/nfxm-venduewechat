54883f66d513dd49540c2ae0adf9898409eefe96
zs97 <1016514880@qq.com>
2019-02-28 14:37:04 +0800

    查询页面样式和按钮已经修改图片中文名字为英文名字

--- src/components/dataQuery/dataQuery.vue
+++ src/components/dataQuery/dataQuery.vue
@@ -7,14 +7,25 @@
       <span style="padding-right: 0.7rem;">查询</span>
     </header>
     <div>
-        <div @click.prevent="entrustSelect" class="entrustSelect">
-            <p>委托查询</p><img src="../../style/zs/images/向右.png">
+        <div @click.prevent="entrustSelect" class="firstSelect">
+            <p>委托查询</p><img src="../../style/zs/images/right.png">
         </div>
-        <div @click.prevent="tradeSelect" class="tradeSelect">
-            <p>成交查询</p><img src="../../style/zs/images/向右.png">
+        <div @click.prevent="tradeSelect" class="secSelect">
+            <p>成交查询</p><img src="../../style/zs/images/right.png">
         </div>
-        <div @click.prevent="contractSelect" class="contractSelect">
-            <p>合同查询</p><img src="../../style/zs/images/向右.png">
+        <div @click.prevent="contractSelect" class="secSelect">
+            <p>合同查询</p><img src="../../style/zs/images/right.png">
+        </div>
+    </div>
+    <div class="sec-Div">
+        <div class="secSelect">
+            <p>挂单查询</p><img src="../../style/zs/images/right.png">
+        </div>
+        <div  class="secSelect">
+            <p>提货单查询</p><img src="../../style/zs/images/right.png">
+        </div>
+        <div  class="secSelect">
+            <p>验收单查询</p><img src="../../style/zs/images/right.png">
         </div>
     </div>
   </div>
